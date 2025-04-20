45cc98d5790b7ff41cbb9799e04b22201cec8ca6
SilvanaSilva83 <lis-carvalho@hotmail.com>
2025-04-20 16:30:23 +0100

    Add files via upload

---
+++ _Membros_Estaticos_DesafioBarOO/Bill.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _Membros_Estaticos_DesafioBarOO
+{
+    internal class Bill
+    {
+        //Attributes
+        public static char gender=' ';
+        public static int beer;
+        public static int barbecue;
+        public static int softDrink;
+        
+
+        //Methds
+
+        public static double Feeding(double beer, double barbecue, double softDrink)
+        {
+            //Valores
+            double totalBeer = beer * 5.00;
+            double totalBarbecue = barbecue * 7.00;
+            double totalSoftDrink = softDrink * 3.00;
+
+            //Calculo
+            return totalBeer + totalBarbecue + totalSoftDrink;
+        }
+
+        public static double Courvert(double feeding)
+        {
+            return feeding > 30 ? 0 : 4; //consumo>30 reais, o couvert artístico = 0.     
+        }
+
+        public static double Ticket(char gender)
+        {
+            return gender == 'M' ? 10 : 8;
+        }
+
+        public static double TotalBill(double feeding, double couvert, double ticket)
+        {
+            return feeding + couvert + ticket;
+        }
+
+
+
+    }
+}
---
+++ _Membros_Estaticos_DesafioBarOO/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Sockets;
+using System.Reflection;
+
+namespace _Membros_Estaticos_DesafioBarOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer um programa para ler os seguintes dados de
+            Console.Write("Sexo: \t\t\t\t");
+            Bill.gender = char.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade de cervejas: \t");
+            Bill.beer = int.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade de refrigerantes: \t");
+            Bill.softDrink = int.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade de espetinhos: \t");
+            Bill.barbecue = int.Parse(Console.ReadLine());
+
+            // Chamar os métodos
+            double Feeding = Bill.Feeding
+                (Bill.beer, Bill.barbecue, Bill.softDrink); //Total do consumo
+            double Couvert = Bill.Courvert(Feeding);
+            double Ticket = Bill.Ticket(Bill.gender);//Valor do ingresso
+            double Total = Bill.TotalBill(Feeding, Couvert, Ticket);
+
+            Console.WriteLine("\n--------- RELATORIO ---------\n");
+
+            Console.WriteLine($"Valor consumo \t= \t\tR$ {Feeding.ToString("F2", CultureInfo.InvariantCulture)}");             
+
+            Console.WriteLine($"Couvert \t= \t\tR$ {Couvert.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine($"Ingressos \t= \t\tR$ {Ticket.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine($"Valor a pagar \t= \t\tR$ {Total.ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+        }
+    }
+}
