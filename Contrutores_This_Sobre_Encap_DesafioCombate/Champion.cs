604b87b65d157f3fc4e5d9e7db4e878d59c86655
SilvanaSilva83 <lis-carvalho@hotmail.com>
2025-04-21 14:14:55 +0100

    Add files via upload

---
+++ Contrutores_This_Sobre_Encap_DesafioCombate/Champion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contrutores_This_Sobre_Encap_DesafioCombate
+{
+    internal class Champion
+    {
+        //Atributos
+        public string Name { get; set; }
+        public int Life { get; set; }
+        public int Attack { get; set; }
+        public int Armor { get; set; }
+
+        //Construtores           // Analogia = Um forma de bolo pra diferentes sabores de bolo
+        public Champion(string name, int life, int attack, int armor)
+        {
+            this.Name = name;
+            this.Life = life;
+            this.Attack = attack;
+            this.Armor = armor;
+        }//Sei que o this nao e necessario, mas por uma questao de didatica prefiro incluir
+
+        //Métodos
+        public void TakeDamage(Champion other)
+        {
+            int damage = other.Attack - this.Armor;
+            if (damage < 1)
+            {
+                damage = 1;
+            }
+
+            this.Life -= damage;
+
+            if (this.Life < 0)
+            {
+                this.Life = 0;                
+            }
+        }
+
+        public string Status()
+        { 
+            if (this.Life > 0)
+            {
+                return this.Name+" Esta vivo";
+            }
+            else
+            {
+                return this.Name+" Esta morto";
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return "Nome: "
+                + Name
+                + "\t"
+                + Life
+                + " de vida";               
+        }
+    }
+}
---
+++ Contrutores_This_Sobre_Encap_DesafioCombate/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Contrutores_This_Sobre_Encap_DesafioCombate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciação de um objeto
+            Champion champion1; 
+            Champion champion2;
+
+            //Inputacao de dados
+            Console.WriteLine("CAMPEÕES");
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine("Digite os dados do Primeiro Campeao");
+            Console.Write("Nome: \t");
+            string name = Console.ReadLine();
+            Console.Write("Vida Inicial: \t");
+            int life = int.Parse(Console.ReadLine());
+            Console.Write("Ataque: \t");
+            int attack = int.Parse(Console.ReadLine());
+            Console.Write("Armadura: \t");
+            int armor = int.Parse(Console.ReadLine());
+            champion1 = new Champion(name, life, attack, armor);
+
+            Console.WriteLine("\n------------------------------\n");
+
+            Console.WriteLine("Digite os dados do Segundo Campeao");
+            Console.Write("Nome: \t");
+            name = Console.ReadLine();
+            Console.Write("Vida Inicial: \t");
+            life = int.Parse(Console.ReadLine());
+            Console.Write("Ataque: \t");
+            attack = int.Parse(Console.ReadLine());
+            Console.Write("Armadura: \t");
+            armor = int.Parse(Console.ReadLine());
+            champion2 = new Champion(name, life, attack, armor);
+
+            Console.WriteLine("\n------------------------------\n");
+            Console.WriteLine("Quantos turnos voce deseja executar: ");
+            int rodadas = int.Parse(Console.ReadLine());            
+            Console.WriteLine("------------------------------");
+
+            // Lógica do combate e exibição do resultado e status
+            for (int i = 1; i <= rodadas; i++)
+            {
+                if (champion1.Life == 0 || champion2.Life == 0)
+                {
+                    break;
+                }
+
+                champion1.TakeDamage(champion2);                
+                champion2.TakeDamage(champion1);
+
+                Console.WriteLine("Rodada " + i);
+                Console.WriteLine(champion1.ToString());                
+                Console.WriteLine(champion2.ToString());
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Fim do combate!");
+            Console.WriteLine(champion1.Status());
+            Console.WriteLine(champion2.Status());
+
+        }
+    }
+}
+   
