2c5ab5c484bbfac981945876259c43212f3b030f
SilvanaSilva83 <lis-carvalho@hotmail.com>
2025-04-23 11:42:26 +0100

    Add files via upload

---
+++ Mem_Vet_LIST_Matriz_ModParametros_DesafioContribuintes/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Xml.Linq;
+using System.Collections.Generic;
+
+namespace Mem_Vet_LIST_Matriz_ModParametros_DesafioContribuintes
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {         
+            Console.Write("Quantos contribuintes voce vai digitar?\t");
+            int numContribuintes = int.Parse(Console.ReadLine());
+            
+            List<TaxPayer> contribuintes = new List<TaxPayer>();
+            
+            Console.WriteLine("Digite os dados do 1o. Contribuinte: ");
+            for (int i = 0; i < numContribuintes; i++)
+            {
+                bool check = false;
+                while (!check)
+                {
+                    Console.WriteLine($"Contribuinte #{i + 1}");
+
+                    Console.Write("Renda anual com salario:\t");
+                    double salaryIncome = double.Parse(Console.ReadLine());
+
+                    Console.Write("Renda anual com prestacao de servicos:\t");
+                    double servicesIncome = double.Parse(Console.ReadLine());
+
+                    Console.Write("Renda anual com ganho de capital:\t");
+                    double capitalIncome = double.Parse(Console.ReadLine());
+
+                    Console.Write("Gasto medicos: ");
+                    double healthSpending = double.Parse(Console.ReadLine());
+
+                    Console.Write("Gasto educacao: ");
+                    double educationSpending = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\n---------- Confirme os dados digitados ----------\n");
+                    Console.WriteLine($"Renda anual com salario: {salaryIncome}");
+                    Console.WriteLine($"Renda anual com prestacao de servicos: {servicesIncome}");
+                    Console.WriteLine($"Renda anual com ganho de capital: {capitalIncome}");
+                    Console.WriteLine($"Gasto medicos: {healthSpending}");
+                    Console.WriteLine($"Gasto educacao: {educationSpending}");
+                    Console.WriteLine("\nConfirmar? (S/N)");
+                    char confirmar = char.Parse(Console.ReadLine().ToUpper());
+                    if (confirmar == 'S')
+                    {
+                        contribuintes.Add(new TaxPayer(salaryIncome, servicesIncome, capitalIncome, healthSpending, educationSpending)); // List
+                     check = true;                    
+                    }
+                    else
+                    {
+                        Console.WriteLine("Digite os dados novamente.");
+                    }
+                    Console.WriteLine("");                                        
+                }                
+            }            
+
+            foreach (TaxPayer contribuinte in contribuintes)
+            {
+                int count = 1;
+                Console.WriteLine($"Resumo do {count}º contribuinte:");
+                Console.WriteLine("Imposto bruto total: " + contribuinte.GrossTax.ToString("F2"));
+                Console.WriteLine("Abatimento: " + contribuinte.TotalSpending.ToString("F2"));
+                Console.WriteLine("Imposto devido: " + (contribuinte.NetTax.ToString("F2")));
+            }
+
+        }
+    }
+}
---
+++ Mem_Vet_LIST_Matriz_ModParametros_DesafioContribuintes/TaxPayer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mem_Vet_LIST_Matriz_ModParametros_DesafioContribuintes
+{
+    internal class TaxPayer
+    {
+        public double SalaryIncome { get; set; }
+        public double ServicesIncome { get; set; }
+        public double CapitalIncome { get; set; }
+        public double HealthSpending { get; set; }
+        public double EducationSpending { get; set; }
+
+        //Construtor
+
+        public TaxPayer(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending, double educationSpending)
+        {
+            this.SalaryIncome = salaryIncome;
+            this.ServicesIncome = servicesIncome;
+            this.CapitalIncome = capitalIncome;
+            this.HealthSpending = healthSpending;
+            this.EducationSpending = educationSpending;
+        }
+
+        //Propriedades
+        public double GrossTax => SalaryTax() + ServicesTax + CapitalTax;        
+        public double TotalIncome => SalaryIncome + ServicesIncome + CapitalIncome;        
+        public double TotalSpending => HealthSpending + EducationSpending;
+        public double TaxRebate => TotalSpending > GrossTax * 0.30 ? GrossTax * 0.30 : TotalSpending;
+        public double ServicesTax => ServicesIncome > 0 ? ServicesIncome * 0.15 : 0.0;
+        public double CapitalTax => CapitalIncome > 0 ? CapitalIncome * 0.20 : 0;
+        public double NetTax => GrossTax - TaxRebate;
+
+
+        //Metodo
+        public double SalaryTax()
+        {
+            if (SalaryIncome < 3000*12) return 0.0;
+            else if (SalaryIncome < 5000*12) return SalaryIncome * 0.10;
+            else return SalaryIncome * 0.20; 
+        }      
+    }
+}
